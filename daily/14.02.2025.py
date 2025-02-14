017262668b1c8456f068954053850642358503fe
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2025-02-14 08:17:13 +0500

    14.02.2025

--- daily/14.02.2025.py
+++ daily/14.02.2025.py
@@ -1,15 +1,29 @@
-class ProductOfNumbers(object):
-    listProd=[]
+class ProductOfNumbers:
+
     def __init__(self):
-        self.listProd.append(1)
-    def add(self, num):
-        if num==0:
-            self.listProd=[]
-            self.listProd.append(1)
+        self.lastZeroIndex = -1
+        self.index = -1
+        self.prefixProduct = []
+
+    def add(self, num: int) -> None:
+        self.index += 1
+        if num == 0:
+            self.lastZeroIndex = self.index
+            self.prefixProduct.append(num)
         else:
-            self.listProd.append(self.listProd[-1]*num)
-    def getProduct(self, k):
-        if k>len(self.listProd)-1:
+            if not self.prefixProduct or self.prefixProduct[-1] == 0:
+                self.prefixProduct.append(num)
+            else:
+                self.prefixProduct.append(self.prefixProduct[-1] * num)
+
+    def getProduct(self, k: int) -> int:
+        if self.index - k < self.lastZeroIndex:
             return 0
         else:
-            return self.listProd[-1]/self.listProd[-1-k]
+            return self.prefixProduct[-1] // (self.prefixProduct[self.index - k] if self.prefixProduct[self.index - k] != 0 and self.index - k >= 0 else 1)
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
